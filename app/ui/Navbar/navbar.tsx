21525e1100f1c213a6599bd5504484bd9d315943
Kareem Sakr <kareem.h.sakr@gmail.com>
2024-11-29 12:46:35 -0500

    Removed a comment

--- app/ui/Navbar/navbar.tsx
+++ app/ui/Navbar/navbar.tsx
@@ -27,19 +27,6 @@ const Navbar = () => {
     <nav className={styles.navbar}>
       <BrandButton />
       <button className={clsx(styles.hamburger_menu)} onClick={toggleMenu}>
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="inline-block h-5 w-5 stroke-current"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg> */}
         ☰
       </button>
       {(isOpen || isClosing) && (
