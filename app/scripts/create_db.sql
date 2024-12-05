c88ffa86267dee9b8b4b2482a8a684dc518a2cf6
Kareem Sakr <kareem.h.sakr@gmail.com>
2024-12-05 15:46:00 -0500

    added a preliminary db script for creating the profile table

--- app/scripts/create_db.sql
+++ app/scripts/create_db.sql
@@ -8,3 +8,15 @@ CREATE TABLE IF NOT EXISTS users (
       password TEXT NOT NULL
     );
 
+CREATE TABLE IF NOT EXISTS profile (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        user_id UUID NOT NULL,
+        bio TEXT,
+        looking_for TEXT,
+        interests TEXT,
+        tagline TEXT,
+        location TEXT,
+        avatar TEXT,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        UNIQUE(user_id),
+);
