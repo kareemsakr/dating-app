f150d4049b111c8ce8cb5482a3d5e15e57472853
Kareem Sakr <kareem.h.sakr@gmail.com>
2024-12-12 13:48:25 -0500

    created db scripts for matches and match requests

--- app/scripts/create_db.sql
+++ app/scripts/create_db.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS users (
       email TEXT NOT NULL UNIQUE,
       birthdate DATE NOT NULL,
       password TEXT NOT NULL,
-    --   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- TODO: Run migration
-      is_active BOOLEAN DEFAULT TRUE, -- TODO: Run migration
+      created_at TIMESTAMP,
+      is_active BOOLEAN DEFAULT TRUE,
       gender VARCHAR(255),
       is_admin BOOLEAN DEFAULT FALSE,
       CONSTRAINT gender_check CHECK (gender IN ('male', 'female', 'other'))
@@ -22,22 +22,43 @@ CREATE TABLE IF NOT EXISTS profile (
         location TEXT,
         avatar_url TEXT,
         non_negotiables TEXT,
-        -- last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- TODO: Run migration
-        is_active BOOLEAN DEFAULT TRUE
+        is_active BOOLEAN DEFAULT TRUE,
+        created_at TIMESTAMP,
 );
 
+CREATE TABLE IF NOT EXISTS match_requests (
+    user_id UUID,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    created_at TIMESTAMP,
+    status VARCHAR(255),
+    CONSTRAINT status_check CHECK (status IN ('pending', 'closed')),
+    notes TEXT,
+    is_active BOOLEAN DEFAULT TRUE
+);
 
-CREATE OR REPLACE FUNCTION update_last_updated_column()
+CREATE TABLE IF NOT EXISTS  matches (
+    user1 UUID PRIMARY KEY,
+    FOREIGN KEY (user1) REFERENCES users(id),
+    user2 UUID,
+    FOREIGN KEY (user2) REFERENCES users(id),
+    UNIQUE (user1, user2),
+    created_at TIMESTAMP,
+    expires_at TIMESTAMP,
+    is_active BOOLEAN DEFAULT TRUE
+);
+
+-- Function to set created_at timestamp
+CREATE OR REPLACE FUNCTION set_created_at()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.last_updated = CURRENT_TIMESTAMP;
+    NEW.created_at = CURRENT_TIMESTAMP;
     RETURN NEW;
 END;
 $$ language 'plpgsql';
 
--- Then create the trigger
-CREATE TRIGGER update_profile_last_updated
-    BEFORE UPDATE
-    ON profile
+-- Example of applying trigger to a table
+-- You'll need to repeat this for each table
+CREATE TRIGGER set_created_at_timestamp
+    BEFORE INSERT ON your_table_name
     FOR EACH ROW
-    EXECUTE FUNCTION update_last_updated_column();
+    EXECUTE FUNCTION set_created_at();
